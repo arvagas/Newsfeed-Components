ef1fd51d0d750bf8a08ef835e1db86b8126b10a4
Arvin Agas <arvagas@gmail.com>
2019-06-19 19:08:00 -0400

    added basic menu animation to menu via GSAP

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -2,6 +2,17 @@
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence.
   menu.classList.toggle('menu--open')
+  // TweenMax.to(target, duration, {value(s)})
+  if (menu.classList.length > 1) {
+    TweenMax.to(menu, 1, {
+      left: 0
+    })
+  }
+  else {
+    TweenMax.to(menu, 1, {
+      left: '-400px'
+    })
+  }
 }
 
 // Start Here: Create a reference to the ".menu" class
