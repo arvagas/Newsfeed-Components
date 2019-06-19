b9271d422cd9610d41c1ed831cc2f5e9f7f3c9c0
Arvin Agas <arvagas@gmail.com>
2019-06-19 17:54:23 -0400

    added close button to articles

--- Article/Article.js
+++ Article/Article.js
@@ -6,15 +6,28 @@ class Article {
     this.domElement = domElement
     // create a reference to the ".expandButton" class. 
     this.expandButton = domElement.querySelector('.expandButton')
+    this.closeButton = domElement.querySelector('.closeButton')
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = 'expand'
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle())
+    this.closeButton.addEventListener('click', () => this.closeArticle())
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open')
+    if (this.expandButton.textContent === 'expand') {
+      this.expandButton.textContent = 'collapse'
+    }
+    else if (this.expandButton.textContent === 'collapse') {
+      this.expandButton.textContent = 'expand'
+    }
+  }
+
+  closeArticle() {
+    this.domElement.classList.toggle('article-open')
+    this.expandButton.textContent = 'expand'
   }
 }
 
@@ -31,3 +44,6 @@ let articles = document.querySelectorAll('.article')
 articles.forEach(item => {
   new Article(item)
 })
+
+// @@@@@@@@@@@@@@@@@@@@ Stretch Goals @@@@@@@@@@@@@@@@@@@@
+
