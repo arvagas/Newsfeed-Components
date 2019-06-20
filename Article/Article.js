849145fb41e2c250b8f68ba7d6c2fe391ce9be04
Arvin Agas <arvagas@gmail.com>
2019-06-20 03:44:51 -0400

    Removed all instances of GSAP -decided to use CSS animations as opposed to importing Navigation transition working Article animation working w/ scroll overflow Utilized existing .close class

--- Article/Article.js
+++ Article/Article.js
@@ -6,7 +6,7 @@ class Article {
     this.domElement = domElement
     // create a reference to the ".expandButton" class. 
     this.expandButton = domElement.querySelector('.expandButton')
-    this.closeButton = domElement.querySelector('.closeButton')
+    this.closeButton = domElement.querySelector('.close')
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = 'expand'
     // Set a click handler on the expandButton reference, calling the expandArticle method.
@@ -16,18 +16,26 @@ class Article {
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
+    // console.log(this.domElement.style.height)
+    // if (this.domElement.scrollHeight > this.domElement.style.height) {
+    //   console.log(this.domElement.style.height)
+    //   this.domElement.style.height = `${this.domElement.scrollHeight}px`
+    // }
+    
     this.domElement.classList.toggle('article-open')
+
     if (this.expandButton.textContent === 'expand') {
       this.expandButton.textContent = 'collapse'
     }
     else if (this.expandButton.textContent === 'collapse') {
       this.expandButton.textContent = 'expand'
     }
+    console.log(this.domElement.scrollHeight)
+    console.log(this.domElement.style.height)
   }
 
   closeArticle() {
-    this.domElement.classList.toggle('article-open')
-    this.expandButton.textContent = 'expand'
+    this.domElement.style.display = 'none'
   }
 }
 
@@ -44,6 +52,3 @@ let articles = document.querySelectorAll('.article')
 articles.forEach(item => {
   new Article(item)
 })
-
-// @@@@@@@@@@@@@@@@@@@@ Stretch Goals @@@@@@@@@@@@@@@@@@@@
-
