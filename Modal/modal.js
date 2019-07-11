c40078e9fcfd74221bbf93729ba0ae7355280e06
Arvin Agas <arvagas@gmail.com>
2019-07-11 11:14:01 -0400

    modal overlay working, modal coming along but still wip

--- Modal/modal.js
+++ Modal/modal.js
@@ -3,9 +3,11 @@ document.querySelector('body').appendChild(modalArticleInput())
 
 // opens a modal for user input
 function modalArticleInput() {
-  //Define new elements
+  // Define new elements
   const modal = document.createElement('div')
-  const modalContent = document.createElement('div')
+  const modalHeader = document.createElement('div') // Modal title, close button
+  const modalTitle = document.createElement('h1')
+  const modalContent = document.createElement('div') // User inputted items
   const title = document.createElement('h2')
   const titleInput = document.createElement('input')
   const date = document.createElement('h2')
@@ -16,10 +18,13 @@ function modalArticleInput() {
   const paraTwoInput = document.createElement('input')
   const paraThree = document.createElement('h2')
   const paraThreeInput = document.createElement('input')
+  const modalFooter = document.createElement('div') // Close, save button 
   const closeBtn = document.createElement('span')
   const saveBtn = document.createElement('span')
 
   // Structure of elements
+  modal.appendChild(modalHeader)
+  modalHeader.appendChild(modalTitle)
   modal.appendChild(modalContent)
   modalContent.appendChild(title)
   modalContent.appendChild(titleInput)
@@ -31,10 +36,12 @@ function modalArticleInput() {
   modalContent.appendChild(paraTwoInput)
   modalContent.appendChild(paraThree)
   modalContent.appendChild(paraThreeInput)
-  modalContent.appendChild(closeBtn)
-  modalContent.appendChild(saveBtn)
+  modal.appendChild(modalFooter)
+  modalFooter.appendChild(closeBtn)
+  modalFooter.appendChild(saveBtn)
 
   // Set attributes
+  modalTitle.textContent = 'Create a New Article'
   title.textContent = 'Title:'
   titleInput.type = 'text'
   date.textContent = 'Date:'
@@ -50,15 +57,33 @@ function modalArticleInput() {
 
   // Set classes
   modal.classList.add('modal')
+  modalHeader.classList.add('modal-header')
   modalContent.classList.add('modal-content')
+
 
 
   return modal
 }
 
+const modalBackdrop = document.createElement('div')
+modalBackdrop.classList.add('modal-backdrop', 'fade')
+
 // Give 'New Article' menu item an event
 const newArticle = document.querySelector('li:nth-last-child(2)')
 newArticle.addEventListener('click', () =>{
-  document.querySelector('.modal').classList.toggle('show')
   document.querySelector('body').classList.toggle('modal-open')
+  document.querySelector('body').appendChild(modalBackdrop)
+  modalBackdrop.classList.toggle('show')
+  document.querySelector('.modal').classList.toggle('modal-show')
+})
+
+modalBackdrop.addEventListener('click', () =>{
+  document.querySelector('body').classList.toggle('modal-open')
+  document.querySelector('body').removeChild(modalBackdrop)
+  modalBackdrop.classList.toggle('show')
+  document.querySelector('.modal').classList.toggle('modal-show')
+})
+
+window.addEventListener('click', event => {
+    console.log(event.target)
 })
